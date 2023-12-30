890bc990ba64506a9a19104a80d2c69bc19aa90e
Justin Lopato <justin@lopato.org>
2023-12-30 13:52:05 -0600

    actually yeah

--- src/models/task2.ts
+++ src/models/task2.ts
@@ -2,15 +2,15 @@ const Temperatures = {
   cel: "",
   fah: "",
   setFah: (value: string) => {
-    Temperatures.cel = value;
-    const c = parseFloat(Temperatures.cel);
+    const c = parseFloat(value);
     if (isNaN(c)) return;
+    Temperatures.cel = value;
     Temperatures.fah = (c * (9.0 / 5.0) + 32).toString();
   },
   setCel: (value: string) => {
-    Temperatures.fah = value;
-    const f = parseFloat(Temperatures.fah);
+    const f = parseFloat(value);
     if (isNaN(f)) return;
+    Temperatures.fah = value;
     Temperatures.cel = ((f - 32) * (5.0 / 9.0)).toString();
   },
 };

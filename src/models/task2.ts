572a37693d1ba0aa2b32cde89320801b3ceafdb5
Justin Lopato <justin@lopato.org>
2023-12-30 13:51:21 -0600

    nvm

--- src/models/task2.ts
+++ src/models/task2.ts
@@ -2,15 +2,15 @@ const Temperatures = {
   cel: "",
   fah: "",
   setFah: (value: string) => {
+    Temperatures.cel = value;
     const c = parseFloat(Temperatures.cel);
     if (isNaN(c)) return;
-    Temperatures.cel = value;
     Temperatures.fah = (c * (9.0 / 5.0) + 32).toString();
   },
   setCel: (value: string) => {
+    Temperatures.fah = value;
     const f = parseFloat(Temperatures.fah);
     if (isNaN(f)) return;
-    Temperatures.fah = value;
     Temperatures.cel = ((f - 32) * (5.0 / 9.0)).toString();
   },
 };
